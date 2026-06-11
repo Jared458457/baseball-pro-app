772f9530061380b130fbd0eb1213ded63651cbcb
Jared458457 <jaredc.smith@yahoo.com>
2026-06-11 13:30:16 -0300

    Update engine.py

--- engine.py
+++ engine.py
@@ -49,38 +49,3 @@ def build_model():
                 model.Add(x[g,i,p,4] == 0)
 
     return model, x
-
-def add_objective(model, x):
-
-    total = []
-
-    for p in range(13):
-        tp = sum(x[g,i,p,pos]
-                 for g in range(14)
-                 for i in range(6)
-                 for pos in range(9))
-        total.append(tp)
-
-    avg = (14*6*9)//13
-
-    model.Minimize(sum((tp - avg)*(tp - avg) for tp in total))
-
-def add_pitching(model, x):
-
-    pitch_plan = [
-        (2,3,5),(8,9,11),(12,2,3),(5,8,9),
-        (11,12,2),(3,5,8),(9,11,12),(2,3,5),
-        (8,9,11),(12,2,3),(5,8,9),(11,12,2),
-        (3,5,8),(9,11,12)
-    ]
-
-    for g in range(14):
-        p1,p2,p3 = pitch_plan[g]
-
-        for i in range(6):
-            if i < 2:
-                model.Add(x[g,i,p1,0] == 1)
-            elif i < 4:
-                model.Add(x[g,i,p2,0] == 1)
-            else:
-                model.Add(x[g,i,p3,0] == 1)
